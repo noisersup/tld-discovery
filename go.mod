e9c2972e7cc6ec6bea4608ed494cc7785aa8026b
noisersup <patekiapl@gmail.com>
2021-04-09 13:36:58 +0200

    :rocket: Release

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/noisersup/tld-discovery
+
+go 1.16
--- main.go
+++ main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
+	"net"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main(){
-	if tlds, err := getLines("tlds-alpha-by-domain.txt") ; err != nil {
+	if len(os.Args) < 2 {
+		log.Fatalf("Provide target as argument ")
+	}
+	tlds, err := getLines("tlds-alpha-by-domain.txt") ; 
+	if err != nil {
 		log.Fatal(err)
+	}
+
+	length := len(tlds)
+	
+	var succeedTlds []string
+	for i,tld := range tlds {
+		if err := ping(os.Args[1]+"."+tld,80); err == nil {
+			succeedTlds = append(succeedTlds, tld)
+		}
+		
+		fmt.Print("\033[H\033[2J")
+		for _,sTld := range succeedTlds {
+			fmt.Printf("%s.%s\n",os.Args[1],sTld)
+		}
+		fmt.Printf("%d/%d\n",i,length)
 	}
 }
 
@@ -27,4 +50,9 @@ func getLines(path string) ([]string, error){
 		return nil, err
 	}
 	return lines,nil
+}
+
+func ping(uri string,port int) (error){
+	if _,err := net.DialTimeout("tcp",uri+":"+strconv.Itoa(port),1 * time.Second) ; err != nil { return err}
+	return nil
 }
